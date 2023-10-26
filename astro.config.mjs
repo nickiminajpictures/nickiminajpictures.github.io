9753b8d9871132dd0ac708a8dbbc305bc1611659
Yes <yes@yes.com>
2023-10-26 11:13:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://nickiminajsexypics.github.io',
+    site: 'https://nickiminajpictures.github.io',
   })
